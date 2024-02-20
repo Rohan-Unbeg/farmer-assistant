d462b4b4ed29a8c5e091b4173342f4e56ea4170e
Rohan-Unbeg <rohanunbeg0918@gmail.com>
2024-02-21 00:15:32 +0530

    added mongodb atlas

--- routes/users.js
+++ routes/users.js
@@ -1,14 +1,16 @@
-const mongoose = require('mongoose');
-const plm = require('passport-local-mongoose'); 
+const mongoose = require("mongoose");
+const plm = require("passport-local-mongoose");
 
-mongoose.connect("mongodb://127.0.0.1:27017/testing")
-
+// mongoose.connect("mongodb://127.0.0.1:27017/testing")
+mongoose.connect(
+  "mongodb+srv://rohan:rohan%23farmer@cluster0.ocinjfj.mongodb.net/?retryWrites=true&w=majority");
+  
 const userSchema = mongoose.Schema({
   username: String,
   password: String,
   secret: String,
-})
+});
 
 userSchema.plugin(plm);
 
-module.exports = mongoose.model("user", userSchema)
+module.exports = mongoose.model("user", userSchema);
